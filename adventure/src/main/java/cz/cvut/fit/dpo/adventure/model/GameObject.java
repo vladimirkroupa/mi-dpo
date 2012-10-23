e1d7a23443ba3afa65b54ffeb75b9ed9efe8c9dc
stoupa <vladimirkroupa@gmail.com>
2012-10-23 09:16:35 +0200

    work in progress

--- adventure/src/main/java/cz/cvut/fit/dpo/adventure/model/GameObject.java
+++
@@ -1,8 +0,0 @@
-package cz.cvut.fit.dpo.adventure.model;
-
-
-public interface GameObject extends GameObjectState, HasName, HasDescription {
-
-	ILocation location();
-	
-}
--- adventure/src/main/java/cz/cvut/fit/dpo/adventure/model/strategy/UseOnStrategy.java
+++
@@ -1,9 +0,0 @@
-package cz.cvut.fit.dpo.adventure.model.strategy;
-
-import cz.cvut.fit.dpo.adventure.model.GameObject;
-
-public interface UseOnStrategy {
-
-	void use(GameObject what, GameObject on);
-	
-}
